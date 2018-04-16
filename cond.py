edd790ee85e6e1ef00d7ba7591f6357f5672a1bb
Omot <yusmot01@gmail.com>
2018-04-16 06:49:37 +0100

    Add some conditional statement

---
+++ cond.py
@@ -0,0 +1,36 @@
+"""
+the lines below handles my must-do before going out
+by keeping my door closed until i am done with the tasks
+the door just opens immediately all the task are completed
+i am trying self discipline
+
+"""
+#first task check
+
+if brushed and prayed:
+	print('first task done')
+else !brushed or !prayed:
+	print('the first task needs to be completed')
+
+#second task check
+
+if made_my_bed and swept:
+	print('second task done')
+else if made_my_bed and !swept:
+	print('You should sweep the floor')
+else if !made_my_bed and swept:
+	print('You should make your bed')
+else:
+	print('your second task needs to be done')
+
+#third task check 
+
+if ate or drank:
+	print('You are now good to go')
+else:
+	print('You should eat or drink something')
+
+if steppedOut:
+	alert('Have a nice day')
+else:
+	return 0 
