874ddfe582ed6fb88ba82d1c5deb9a82d6bdd8f4
Omot <yusmot01@gmail.com>
2018-04-16 06:51:30 +0100

    Add some loop statement here

---
+++ loops.py
@@ -0,0 +1,16 @@
+#practice on loops
+# for/oops through the first set
+
+for i in range(1, 11):
+	print (i)
+
+numbers = [i for i in range(0,11) ]
+print (numbers)
+
+for number in numbers:
+	if number % 2 == 0:
+		print (number)
+
+#while /oops is next
+
+
